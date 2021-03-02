871d8dd0d2c014b25e0b839fe765c364d44ff7af
Cherno Badjan <badjancherno@gmail.com>
2021-03-02 14:43:14 -0800

    file structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch,
+// } from 'react-router-dom';
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        App
+      </div>
+    )
+  };
+}
